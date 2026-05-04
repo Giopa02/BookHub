ee08e42c385a11f1053b60dd50b8811c8331ae56
Giordana Nyom <giordana.nyom@outlook.fr>
2026-05-04 15:20:29 +0200

    fix: clear caches instead of caching

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 php artisan migrate:fresh --force --seed
-php artisan config:cache
-php artisan route:cache
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
 php-fpm -D
 nginx -g 'daemon off;'
